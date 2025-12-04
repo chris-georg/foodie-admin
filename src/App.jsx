16a9f95995c94b2cb160429167fb0ef368254f4b
chris-georg <chrisgeorge0425@gmail.com>
2025-12-04 16:52:05 +0100

    Change API URL from localhost to production

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://foodie-backend-3p4k.onrender.com";
 
   return (
     <div>
